971b61da08cd0cbfabf11329929fe6c289e0f139
JMCNATIONAL\brismedia <facilitiesbri@jmc.edu.au>
2026-06-16 11:31:54 +1000

    first

---
+++ Assets/Asteroids Assets/Scripts/Ship Script.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ShipScript : MonoBehaviour
+{
+    public float Engine_Power = 10f;
+    public float Turn_Power = -10f;
+
+    private Rigidbody2D rb2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        Apply_Thrust(V);
+        Apply_Torque(H);
+    }
+
+    private void Apply_Thrust(float amount)
+    {
+        Vector2 Thrust = transform.up * Engine_Power * Time.deltaTime * amount;
+        rb2D.AddForce(Thrust);
+    }
+
+    private void Apply_Torque(float amount)
+    {
+        float Torque = amount * Turn_Power * Time.deltaTime;
+        rb2D.AddTorque(Torque);
+    }
+}    
