53b8d18f43bd7365e1c761d204980584f12aa0d5
JMCNATIONAL\brismedia <facilitiesbri@jmc.edu.au>
2026-06-23 09:39:52 +1000

    Gave meteors gravity and added HP to ship.

--- Assets/Asteroids Assets/Scripts/Ship Script.cs
+++ Assets/Asteroids Assets/Scripts/Ship Script.cs
@@ -4,18 +4,22 @@ public class ShipScript : MonoBehaviour
 {
     public float Engine_Power = 10f;
     public float Turn_Power = -10f;
+    public float Max_HP = 3f;
+    public float Current_HP;
 
     private Rigidbody2D rb2D;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Current_HP = Max_HP;
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float H = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
         Apply_Thrust(V);
